3fac0f322af9e495033a10479bccd6dcad5f2dab
Vinayak Palve <vinayak.palve01@gmail.com>
2022-10-01 13:13:10 +0530

    postgres1 commit

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn social_book.wsgi --log-file=-
+web: gunicorn social_book.wsgi --log-file -
--- social_book/settings.py
+++ social_book/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+import django_heroku
+
 import os
-from django import django_heroku
+
+from pathlib import Path
+
 # from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
