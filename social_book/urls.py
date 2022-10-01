11e57d82521e07da5cd0a11007c73abf6acd0ba9
Vinayak Palve <vinayak.palve01@gmail.com>
2022-10-01 11:43:13 +0530

    re_path commit

--- social_book/urls.py
+++ social_book/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
